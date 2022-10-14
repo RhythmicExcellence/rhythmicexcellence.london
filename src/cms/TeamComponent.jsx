c0198b043e41ad0ef267c9fa234cbea951d2bda8
andreasonny83 <andreasonny83@gmail.com>
2022-10-14 10:05:41 +0100

    fix(cms): cms components preview
    Fix Team and Timetable preview components for NetlifyCMS

---
+++ src/cms/TeamComponent.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const Team = ({entry, widgetFor, getAsset}) => {
+  const image = entry.getIn(['data', 'avatar']);
+  const name = entry.getIn(['data', 'title']);
+  const titles = entry.getIn(['data', 'titles']).split('\n');
+  const body = widgetFor('body');
+  const details = widgetFor('details');
+
+  return <div className='team'>
+    <img src={getAsset(image).toString()} alt="profile-image" className="avatar" />
+    <h1>{name.toUpperCase()}</h1>
+    <blockquote>
+      {titles.map((title) => <p>{title}</p>)}
+    </blockquote>
+    <p className='details'>{details}</p>
+    <hr className='separator' />
+    <p className='description'>{body}</p>
+</div>;
+};
