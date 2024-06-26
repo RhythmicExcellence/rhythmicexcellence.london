e134e12625959080a11dd7c5908816ed86644b84
andreasonny83 <andreasonny83@gmail.com>
2024-06-26 13:02:14 +0100

    hide homepage title

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,14 +4,12 @@ import propTypes from 'prop-types';
 import './Homepage.css';
 
 export function Homepage({ node }) {
-  const title = node.frontmatter && node.frontmatter.title;
   const content = node.html;
 
   /* eslint-disable react/no-danger */
   return (
     <div className="Homepage">
       <div className="container content">
-        <h2 className="title">{title}</h2>
         <div dangerouslySetInnerHTML={{ __html: content }} />
       </div>
     </div>
